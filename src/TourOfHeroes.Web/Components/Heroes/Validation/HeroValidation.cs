07fbe87e5a219430dc91ca6f1e3267821520593d
George Mathieson <george@mathieson.me>
2019-09-08 11:28:24 +0100

    Switch to using fluent validation

---
+++ src/TourOfHeroes.Web/Components/Heroes/Validation/HeroValidation.cs
@@ -0,0 +1,20 @@
+using FluentValidation;
+using TourOfHeroes.Data;
+
+namespace TourOfHeroes.Web.Components.Heroes.Validation
+{
+    /// <summary>
+    /// Validation rules for <see cref="Hero" />.
+    /// </summary>
+    public class HeroValidation : AbstractValidator<Hero>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HeroValidation" /> class.
+        /// </summary>
+        public HeroValidation()
+        {
+            RuleFor(hero => hero.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(hero => hero.Name).MaximumLength(50).WithMessage("Name is too long");
+        }
+    }
+}
---
+++ src/TourOfHeroes.Web/Validation/FluentValidator.cs
@@ -0,0 +1,31 @@
+using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace TourOfHeroes.Web.Validation
+{
+    /// <summary>
+    /// Custom validator component which uses Fluent validation.
+    /// </summary>
+    public class FluentValidator : ComponentBase
+    {
+        /// <summary>
+        /// Gets or sets the forms <see cref="EditContext" />.
+        /// </summary>
+        [CascadingParameter]
+        public EditContext EditContext { get; set; }
+
+        /// <summary>
+        /// Initialization of the validator.
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            if (EditContext == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            EditContext.AddFluentValidation();
+        }
+    }
+}
