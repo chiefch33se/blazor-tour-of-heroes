3bb54f28cb690e51e38dd9023c4747bf6c111338
George Mathieson <george@mathieson.me>
2019-09-08 18:05:51 +0100

    Update fluent validation

--- src/TourOfHeroes.Web/Validation/FluentValidator.cs
+++ src/TourOfHeroes.Web/Validation/FluentValidator.cs
@@ -1,4 +1,5 @@
 using System;
+using FluentValidation;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 
@@ -7,25 +8,93 @@ namespace TourOfHeroes.Web.Validation
     /// <summary>
     /// Custom validator component which uses Fluent validation.
     /// </summary>
-    public class FluentValidator : ComponentBase
+    /// <remarks>
+    /// https://github.com/SteveSandersonMS/BlazorUnitTestingPrototype/blob/master/Microsoft.AspNetCore.Components.Testing/TestHost.cs
+    /// </remarks>
+    public class FluentValidator<TValidator> : ComponentBase where TValidator: IValidator, new()
     {
-        /// <summary>
-        /// Gets or sets the forms <see cref="EditContext"/>.
-        /// </summary>
-        [CascadingParameter]
-        public EditContext EditContext { get; set; }
+        private readonly static char[] separators = new[] { '.', '[' };
+        private TValidator validator;
 
-        /// <summary>
-        /// Initialization of the validator.
-        /// </summary>
+        [CascadingParameter] private EditContext EditContext { get; set; }
+
         protected override void OnInitialized()
         {
-            if (EditContext == null)
-            {
-                throw new InvalidOperationException();
-            }
+            validator = new TValidator();
+            var messages = new ValidationMessageStore(EditContext);
 
-            EditContext.AddFluentValidation();
+            // Revalidate when any field changes, or if the entire form requests validation
+            // (e.g., on submit)
+
+            EditContext.OnFieldChanged += (sender, eventArgs)
+                => ValidateModel((EditContext)sender, messages);
+
+            EditContext.OnValidationRequested += (sender, eventArgs)
+                => ValidateModel((EditContext)sender, messages);
+        }
+
+        private void ValidateModel(EditContext editContext, ValidationMessageStore messages)
+        {
+            var validationResult = validator.Validate(editContext.Model);
+            messages.Clear();
+            foreach (var error in validationResult.Errors)
+            {
+                var fieldIdentifier = ToFieldIdentifier(editContext, error.PropertyName);
+                messages.Add(fieldIdentifier, error.ErrorMessage);
+            }
+            editContext.NotifyValidationStateChanged();
+        }
+
+        private static FieldIdentifier ToFieldIdentifier(EditContext editContext, string propertyPath)
+        {
+            // This method parses property paths like 'SomeProp.MyCollection[123].ChildProp'
+            // and returns a FieldIdentifier which is an (instance, propName) pair. For example,
+            // it would return the pair (SomeProp.MyCollection[123], "ChildProp"). It traverses
+            // as far into the propertyPath as it can go until it finds any null instance.
+
+            var obj = editContext.Model;
+
+            while (true)
+            {
+                var nextTokenEnd = propertyPath.IndexOfAny(separators);
+                if (nextTokenEnd < 0)
+                {
+                    return new FieldIdentifier(obj, propertyPath);
+                }
+
+                var nextToken = propertyPath.Substring(0, nextTokenEnd);
+                propertyPath = propertyPath.Substring(nextTokenEnd + 1);
+
+                object newObj;
+                if (nextToken.EndsWith("]"))
+                {
+                    // It's an indexer
+                    // This code assumes C# conventions (one indexer named Item with one param)
+                    nextToken = nextToken.Substring(0, nextToken.Length - 1);
+                    var prop = obj.GetType().GetProperty("Item");
+                    var indexerType = prop.GetIndexParameters()[0].ParameterType;
+                    var indexerValue = Convert.ChangeType(nextToken, indexerType);
+                    newObj = prop.GetValue(obj, new object[] { indexerValue });
+                }
+                else
+                {
+                    // It's a regular property
+                    var prop = obj.GetType().GetProperty(nextToken);
+                    if (prop == null)
+                    {
+                        throw new InvalidOperationException($"Could not find property named {nextToken} on object of type {obj.GetType().FullName}.");
+                    }
+                    newObj = prop.GetValue(obj);
+                }
+
+                if (newObj == null)
+                {
+                    // This is as far as we can go
+                    return new FieldIdentifier(obj, nextToken);
+                }
+
+                obj = newObj;
+            }
         }
     }
 }
