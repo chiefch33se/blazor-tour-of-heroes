2da4e1941dbae4336bb47843b1bc2fbbb0b9bcc2
George Mathieson <george@mathieson.me>
2019-10-18 11:21:44 +0100

    Format document

--- src/TourOfHeroes.Components/Base/BaseHandler.cs
+++ src/TourOfHeroes.Components/Base/BaseHandler.cs
@@ -1,17 +1,16 @@
+using BlazorState;
+using TourOfHeroes.Components.Heroes.State;
+
 namespace MyBlazorApp.Client.Features.Base
 {
-  using BlazorState;
-    using TourOfHeroes.Components.Heroes.State;
-
     /// <summary>
-    /// Base Handler that makes it easy to access state
+    /// Base Handler that makes it easy to access state.
     /// </summary>
-    /// <typeparam name="TAction"></typeparam>
     public abstract class BaseHandler<TAction> : ActionHandler<TAction>
-    where TAction : IAction
-  {
-    protected HeroState _heroState => Store.GetState<HeroState>();
+        where TAction : IAction
+    {
+        protected HeroState _heroState => Store.GetState<HeroState>();
 
-    public BaseHandler(IStore aStore) : base(aStore) { }
-  }
+        public BaseHandler(IStore aStore) : base(aStore) { }
+    }
 }
