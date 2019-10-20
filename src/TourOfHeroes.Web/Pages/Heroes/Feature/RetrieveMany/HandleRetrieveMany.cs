66b8f1b594540ba1d4f2e70b8a26dfe2f801d38c
George Mathieson <george@mathieson.me>
2019-10-20 16:11:49 +0100

    Update ids

--- src/TourOfHeroes.Web/Pages/Heroes/Feature/RetrieveMany/HandleRetrieveMany.cs
+++ src/TourOfHeroes.Web/Pages/Heroes/Feature/RetrieveMany/HandleRetrieveMany.cs
@@ -19,16 +19,16 @@ namespace TourOfHeroes.Web.Pages.Heroes.Feature
                 // TODO: Make service call.
                 var heroes = new List<Hero>
                 {
-                    new Hero{ Id = 111, Name = "Dr Nice" },
-                    new Hero{ Id = 122, Name = "Narco" },
-                    new Hero{ Id = 133, Name = "Bombasto" },
-                    new Hero{ Id = 144, Name = "Celeritas" },
-                    new Hero{ Id = 155, Name = "Magneta" },
-                    new Hero{ Id = 166, Name = "RubberMan" },
-                    new Hero{ Id = 177, Name = "Dynama" },
-                    new Hero{ Id = 188, Name = "Dr IQ" },
-                    new Hero{ Id = 199, Name = "Magma" },
-                    new Hero{ Id = 200, Name = "Tornado" },
+                    new Hero{ Id = 11, Name = "Dr Nice" },
+                    new Hero{ Id = 12, Name = "Narco" },
+                    new Hero{ Id = 13, Name = "Bombasto" },
+                    new Hero{ Id = 14, Name = "Celeritas" },
+                    new Hero{ Id = 15, Name = "Magneta" },
+                    new Hero{ Id = 16, Name = "RubberMan" },
+                    new Hero{ Id = 17, Name = "Dynama" },
+                    new Hero{ Id = 18, Name = "Dr IQ" },
+                    new Hero{ Id = 19, Name = "Magma" },
+                    new Hero{ Id = 20, Name = "Tornado" },
                 };
 
                 _heroesState.Heroes.AddRange(heroes);
