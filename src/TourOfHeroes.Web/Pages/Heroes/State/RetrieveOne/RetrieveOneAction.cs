12d5d69158216a8f1c10f0b599f695278d3ad933
George Mathieson <george@mathieson.me>
2019-11-01 15:24:36 +0000

    Add missing XML comments

--- src/TourOfHeroes.Web/Pages/Heroes/State/RetrieveOne/RetrieveOneAction.cs
+++ src/TourOfHeroes.Web/Pages/Heroes/State/RetrieveOne/RetrieveOneAction.cs
@@ -2,12 +2,23 @@ using BlazorState;
 
 namespace TourOfHeroes.Web.Pages.Heroes.State
 {
-    public partial class HeroesState 
+    /// <inheritdoc/>
+    public partial class HeroesState
     {
+        /// <summary>
+        /// Action for retrieving a single <see cref="Hero"/>.
+        /// </summary>
         protected internal class RetrieveOneAction : IAction
         {
+            /// <summary>
+            /// Gets or sets the ID of the <see cref="Hero"/> to retrieve.
+            /// </summary>
             public int Id { get; private set; }
 
+            /// <summary>
+            /// Initializes a new instance of the <see cref="RetrieveOneAction"/> class.
+            /// </summary>
+            /// <param name="id">The payload to create with.</param>
             public RetrieveOneAction(int id)
             {
                 Id = id;
