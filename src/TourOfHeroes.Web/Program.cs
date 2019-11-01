a58de44910925bb374c938768d038887709998a5
George Mathieson <george@mathieson.me>
2019-11-01 08:14:02 +0000

    Add missing XML comments to startup and program

--- src/TourOfHeroes.Web/Program.cs
+++ src/TourOfHeroes.Web/Program.cs
@@ -3,13 +3,20 @@ using Microsoft.Extensions.Hosting;
 
 namespace TourOfHeroes.Web
 {
+    /// <summary>
+    /// Main program.
+    /// </summary>
     public static class Program
     {
+        /// <summary>
+        /// Main entry point for the application.
+        /// </summary>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <inheritdoc/>        
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
