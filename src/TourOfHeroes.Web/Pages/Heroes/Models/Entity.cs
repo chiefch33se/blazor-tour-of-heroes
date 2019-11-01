064ee10b2edc692f0f39b42ad9551e1852c378b9
George Mathieson <george@mathieson.me>
2019-11-01 15:15:45 +0000

    Update models with equality comparer and entity base

---
+++ src/TourOfHeroes.Web/Pages/Heroes/Models/Entity.cs
@@ -0,0 +1,13 @@
+namespace TourOfHeroes.Web.Pages.Heroes.Models
+{
+    /// <summary>
+    /// Represents an entity within the system.
+    /// </summary>
+    public abstract class Entity
+    {
+        /// <summary>
+        /// Gets or sets the ID.
+        /// </summary>
+        public int Id { get; set; } = 0;
+    }
+}
