8b9e50bc639f0ac47ecd7d7319117cff7f3741c6
George Mathieson <george@mathieson.me>
2019-11-01 15:59:10 +0000

    Add tests for the Hero model

---
+++ tests/TourOfHeroes.Web.Tests/Pages/Heroes/Models/HeroTests.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using TourOfHeroes.Web.Pages.Heroes.Models;
+using Xunit;
+
+namespace TourOfHeroes.Web.Tests.Pages.Heroes.Models
+{
+    /// <summary>
+    /// Unit tests for <see cref="Hero"/>.
+    /// </summary>
+    public class HeroTests
+    {
+        /// <summary>
+        /// Happy path.
+        /// </summary>
+        [Fact]
+        public void Equals_IsEqual_ShouldBeTrue()
+        {
+            // Arrange.
+            var tornado1 = new Hero
+            {
+                Id = 1,
+                Name = "Tornado"
+            };
+            var tornado2 = new Hero
+            {
+                Id = 1,
+                Name = "Tornado"
+            };
+
+            // Act.
+            var results = new List<bool>();
+            results.Add(tornado1.Equals(tornado2));
+            results.Add(tornado1.Equals(tornado2 as object));
+
+            // Assert.
+            Assert.True(results.TrueForAll(item => item));
+        }
+
+        /// <summary>
+        /// Entities not equal should not return true on equality.
+        /// </summary>
+        [Fact]
+        public void Equals_NotEqual_ShouldBeFalse()
+        {
+            // Arrange.
+            var tornado = new Hero
+            {
+                Id = 1,
+                Name = "Tornado"
+            };
+
+            // Act.
+            var results = new List<bool>();
+            results.Add(tornado.Equals(new Hero()));
+            results.Add(tornado.Equals(new object()));
+
+            // Assert.
+            Assert.True(results.TrueForAll(item => item == false));
+        }
+    }
+}
