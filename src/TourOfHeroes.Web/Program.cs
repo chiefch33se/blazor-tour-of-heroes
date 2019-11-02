fcb04455db2e35ad8e083ca78a81ea31eaf90d48
George Mathieson <george@mathieson.me>
2019-11-02 19:08:41 +0000

    Add UseStaticWebAssets for other environments

--- src/TourOfHeroes.Web/Program.cs
+++ src/TourOfHeroes.Web/Program.cs
@@ -6,6 +6,10 @@ namespace TourOfHeroes.Web
     /// <summary>
     /// Main program.
     /// </summary>
+    /// <remarks>
+    /// https://docs.microsoft.com/en-us/aspnet/core/razor-pages/ui-class?view=aspnetcore-3.0&tabs=netcore-cli#consume-content-from-a-referenced-rcl
+    /// https://docs.microsoft.com/en-us/aspnet/core/fundamentals/environments?view=aspnetcore-3.0
+    /// </remarks>
     public static class Program
     {
         /// <summary>
@@ -21,6 +25,9 @@ namespace TourOfHeroes.Web
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    // Static web assets are enabled by default in the Development environment.
+                    // UseStaticWebAssets will support assets in other environments.
+                    webBuilder.UseStaticWebAssets();
                     webBuilder.UseStartup<Startup>();
                 });
     }
