3e9af2f055c2c2c63bb5bee9b6fbb5fb4f766664
George Mathieson <george@mathieson.me>
2019-11-03 16:54:41 +0000

    Allow top heroes to be clickable

--- src/TourOfHeroes.Web/Common/Containers/TopHeroes/TopHeroes.razor.cs
+++ src/TourOfHeroes.Web/Common/Containers/TopHeroes/TopHeroes.razor.cs
@@ -11,6 +11,21 @@ namespace TourOfHeroes.Web.Common.Containers.TopHeroes
     public class TopHeroesBase : BlazorStateComponent
     {
         /// <summary>
+        /// Gets or sets the <see cref="NavigationManager"/>.
+        /// </summary>
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        /// <summary>
+        /// Send the user to the details page.
+        /// </summary>
+        /// <param name="id">The ID of the <see cref="Hero"/> to view details for.</param>
+        protected void NavigateToHeroDetails(int id)
+        {
+            NavigationManager.NavigateTo($"/details/{id}");
+        }
+
+        /// <summary>
         /// Gets the heroes state.
         /// </summary>
         protected HeroesState HeroesState => GetState<HeroesState>();
